a72d6232a0141cb6cc6124f86af7c4156f3a943c
BendersBender <ev.l.waits@gmail.com>
2016-11-04 00:14:37 -0700

    end of night

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -6,6 +6,7 @@ import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import App from './app';
 import ArtistProfileContainer from './artist_profile/artist_profile_container';
 import SessionFormContainer from './session_form/session_form_container';
+import {Splash} from './splash/splash';
 
 const Root = ({store}) => {
   const _ensureLoggedIn = (nextState, replace) => {
@@ -25,6 +26,7 @@ const Root = ({store}) => {
     <Provider store={store}>
       <Router history={hashHistory}>
         <Route path="/" component={App}>
+          <IndexRoute component={Splash} />
           <Route path='/profile' component={ArtistProfileContainer}
             onEnter={_ensureLoggedIn} />
         </Route>
---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const Splash = () => (
+  <div id='splash'>
+    <div className='col-70' id='main-splash'>
+      <h3>
+        Sylvan Esso
+      </h3>
+    </div>
+    <div className='col-30' id='secondary-splash'>
+      <div id='tom-waits' className="small-splash">
+        <h4>
+          Tom Waits
+        </h4>
+      </div>
+      <div id='vince-staples' className="small-splash">
+        <h4>
+          Vince Staples
+        </h4>
+      </div>
+      <div id='pup' className="small-splash">
+        <h4>
+          PUP
+        </h4>
+      </div>
+    </div>
+  </div>
+);
